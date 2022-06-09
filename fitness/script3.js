eeb90e8909d3c2377bfcb23f3b45efa37e882ec8
James Jones <46056989+AceJacobi@users.noreply.github.com>
2022-06-09 10:42:02 -0500

    Update script3.js

--- fitness/script3.js
+++ fitness/script3.js
@@ -1,14 +1,18 @@
 function samePass(){
     
     let a1 = document.getElementById('').value;
-    let a2 = document.getElementById('').value;
     
+    let a2 = document.getElementById('').value;    
     
     if(a1 === a2){
+        
         return true;
+        
     }
     else {
+        
         return false;
+        
     }
     
 }
