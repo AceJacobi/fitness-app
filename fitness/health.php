e09281e72aacf6d2658e2b73d5797e4d7277e8e4
James Jones <46056989+AceJacobi@users.noreply.github.com>
2022-06-09 10:20:57 -0500

    Update health.php

--- fitness/health.php
+++ fitness/health.php
@@ -4,11 +4,14 @@ session_start();
 require_once 'log.php';
 
 
-
 if(isset($_POST['weight'])){
+    
     $uWeight = (int)$_POST['weight'];
+    
     $uAge = (int)$_POST['age'];
+    
     $uHeight = (float)$_POST['height'];
+    
     $gender = $_POST['gButton'];
     
     
@@ -19,20 +22,28 @@ if(isset($_POST['weight'])){
     if($gender === 'M'){
         $bmr = (10*$uWeight) + (6.25*$uHeight) - (5*$uAge) + 5;
     }
+    
     else{
         $bmr = (10*$uWeight) + (6.25*$uHeight) - (5*$uAge) - 161;
     }
     
     $bmr = (int)$bmr;
-    $fat = $bmr/30;
-    $fat = (int)$fat;
-    $protein = .35*$uWeight;
-    $protein = (int)$protein;
-    $cholesterol = 300;
-    $weightLossP = $bmr*.3;
-    $weightLossP = (int)$weightLossP;
-    $sugar = 30;
     
+    $fat = $bmr/30;
+    
+    $fat = (int)$fat;
+    
+    $protein = .35*$uWeight;
+    
+    $protein = (int)$protein;
+    
+    $cholesterol = 300;
+    
+    $weightLossP = $bmr*.3;
+    
+    $weightLossP = (int)$weightLossP;
+    
+    $sugar = 30;    
 
     $query = "Insert into" {column name} "VALUES('$emailS', '$bmr', '$protein', '$fat', '$sugar', '$cholesterol', '$uWeight')";
     
@@ -40,23 +51,19 @@ if(isset($_POST['weight'])){
     
     if($result){
         echo 'Values Updated!';
-        echo <<<_END
-        <script>
-        window.location = './main.php';
-        </script>
         
+        echo <<<_END
+            <script>
+                window.location = './main.php';
+            </script>        
         _END;
     }
     else{
         echo 'Query failed! Please try again.';
-    }    
-    
+    }       
 }
 else {
     die('This page is not available for display!');
 }
 
-    
-    
-    
 ?>
