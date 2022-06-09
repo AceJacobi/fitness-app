83512612273a6d3bb3cf8ed6db3f3e42c48f3c05
James Jones <46056989+AceJacobi@users.noreply.github.com>
2022-06-09 10:23:19 -0500

    Update log.php

--- fitness/log.php
+++ fitness/log.php
@@ -1,7 +1,9 @@
 <?php
 
 $dbhost = {your db host url};
+
 $dbuser = {your db username};
+
 $dbpass = {your db password};
 
 $conn = new mysqli($dbhost, $dbuser, $dbpass);
@@ -11,7 +13,6 @@ if($conn->connect_error) die('Connection unsuccessful!');
 $query = 'use' {yourdbname};
 
 $result = $conn->query($query);
-
 
 if(!$result) die('Database not selected!');
 
