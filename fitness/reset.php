906277f4424dd11cf32961494d61c8844c641fd0
James Jones <46056989+AceJacobi@users.noreply.github.com>
2022-06-09 10:36:12 -0500

    Update reset.php

--- fitness/reset.php
+++ fitness/reset.php
@@ -3,47 +3,68 @@ session_start();
 
 require_once 'log.php';
 
-
-
 if(isset($_POST['weight'])){
-    $uWeight = (int)$_POST['weight'];
-    $uAge = (int)$_POST['age'];
-    $uHeight = (float)$_POST['height'];
-    $gender = $_POST['gButton'];
     
+    $uWeight = (int)$_POST['weight'];
+    
+    $uAge = (int)$_POST['age'];
+    
+    $uHeight = (float)$_POST['height'];
+    
+    $gender = $_POST['gButton'];    
     
     $emailS = $_SESSION['cemail'];
-    
-    //submit data to database after configuring the proper calorie count, protein, and total fat.
-    
+        
     if($gender === 'M'){
+        
         $bmr = (10*$uWeight) + (6.25*$uHeight) - (5*$uAge) + 5;
+        
     }
     else{
+        
         $bmr = (10*$uWeight) + (6.25*$uHeight) - (5*$uAge) - 161;
+        
     }
     
     $bmr = (int)$bmr;
+    
     $fat = $bmr/30;
+    
     $fat = (int)$fat;
+    
     $protein = .35*$uWeight;
+    
     $protein = (int)$protein;
+    
     $cholesterol = 300;
+    
     $weightLossP = $bmr*.3;
+    
     $weightLossP = (int)$weightLossP;
+    
     $sugar = 30;
 
     echo<<<_END
-    weight: $uWeight <br>
-    age: $uAge <br>
-    height: $uHeight <br>
-    cholesterol limit: $cholesterol mg <br>
-    gender: $gender <br>
-    calories: $bmr <br>
-    fat: $fat g<br>
-    minimum daily protein: $protein g<br>
-    calories from protein for weight loss: $weightLossP <br>
-    email: $emailS
+    
+        weight: $uWeight <br>
+        
+        age: $uAge <br>
+        
+        height: $uHeight <br>
+        
+        cholesterol limit: $cholesterol mg <br>
+        
+        gender: $gender <br>
+        
+        calories: $bmr <br>
+        
+        fat: $fat g<br>
+        
+        minimum daily protein: $protein g<br>
+        
+        calories from protein for weight loss: $weightLossP <br>
+        
+        email: $emailS
     
     _END;
     
@@ -55,18 +76,18 @@ if(isset($_POST['weight'])){
     $result = $conn->query($query);
     
     if($result){
-        echo 'Values Updated!';
-        echo <<<_END
-        <script>
-        window.location = './main.php';
-        </script>
         
+        echo 'Values Updated!';
+        
+        echo <<<_END
+            <script>
+                window.location = './main.php';
+            </script>        
         _END;
     }
     else{
         echo 'Query failed! Please try again.';
-    }    
-    
+    }        
 }
 else {
     die('This page is not available for display!');
