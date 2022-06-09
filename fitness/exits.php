168805bb0eb5122756559b9ebf7e17ef90649ff3
James Jones <46056989+AceJacobi@users.noreply.github.com>
2022-06-09 10:15:36 -0500

    Update exits.php

--- fitness/exits.php
+++ fitness/exits.php
@@ -5,15 +5,16 @@ require_once 'log.php';
 require_once 'functions.php';
 
 
-
 destroySession();
 
 echo "Signed Out!";
 
 echo <<<_HOW
-<script>
-window.location = './index.php';
-</script>
+  <script>
+  
+    window.location = './index.php';
+    
+  </script>
 _HOW;
 
 
